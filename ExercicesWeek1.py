2dbe6fc0e03dcbefe9bc8d18fe34081c81f760ae
DvorahC <51906224+DvorahC@users.noreply.github.com>
2019-06-26 09:13:40 +0300

    Update ExercicesWeek1.py

--- ExercicesWeek1.py
+++ ExercicesWeek1.py
@@ -34,7 +34,6 @@ def star(n):
 star(n)
 
 # 4  Write a program that receive a number from the user and print rows of asterisks that extend up to the number entered and added 2.
-
 n1 = int(input('enter a number: '))
 
 
@@ -43,19 +42,17 @@ def function(n1):
     
     for i in range(0, n1 + 3, 2): # the first loop defines the number of rows
         
-        for j in range(1, k): # I understand that this loop is defining the spaces 
-            print(j * " ")
+        for j in range(0, k): # I understand that this loop is defining the spaces 
+            print(end=" ")
         k = k - 2   # we remove 2 spaces at every run of the this loop
         
-        for j in range(0, i +1): # this loop is doing the number of colums
+        for j in range(0, i + 1): # this loop is doing the number of colums
             print("* ", end="")
    
 
         print("\r") # ending line after each row
     
     ## THE ISSUE IS THAT i DONT GET THE EXACT SPACING BETWEEN THE STARS
-
 function(n1)
-
 
 # 5
