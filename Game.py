1bf265d1d1c358b30a30283cea5841f289b9389a
DvorahC <51906224+DvorahC@users.noreply.github.com>
2019-09-04 21:02:43 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -16,7 +16,7 @@ import re
 
 print("Welcome!\n"
       "You are playing against the computer...\t"
-      "Insert a number with 4 digits. You win if you picked the same number as the computer.\n"
+      "Insert a number with 4 unique digits. You win if you picked the same number as the computer.\n"
       "You only have 20 trials.\n"
       "Some hints will be given to you as you are playing the game..."
       "Good Luck!")
@@ -24,18 +24,21 @@ print("Welcome!\n"
 
 # GET THE USER NUMBER AND CONVERT IT INTO A LIST OF DIGITS
 def set_up_user_number():
-    user_number = input('enter a 4 digits number: ')
-    if verify_number_format(user_number):
-        user_numbers_to_list = [int(digit) for digit in user_number]
-        print(f'Your number: {user_numbers_to_list}')
-        return user_numbers_to_list
+    while True:
+        user_number = input('enter a 4 digits number: ')
+        if verify_number_format(user_number):
+            user_numbers_to_list = [int(digit) for digit in user_number]
+            print(f'Your number: {user_numbers_to_list}')
+            return user_numbers_to_list
+        else:
+            continue
 
 
 def verify_number_format(n):
-    if re.match(r"(\d{4})", n):
+    if re.match(r'^\d{4}$', n) and not re.match(r'(\d).*\1', n):
         return True
     else:
-        print("This is not 4 digits number...Try again.")
+        print("This is not 4 digits number without repetition...Try again.")
         return False
 
 
@@ -62,22 +65,31 @@ def play_the_game():
         number_of_set += 1
         b = set_up_user_number()
         check_user_number(b, a)
-        if a == b:
-            print(f'You had the correct number: {a}...Try to improve your score next time!')
-            break
-        elif a != b and number_of_set == 20:
-            print(f'sorry...end of the game, you missed all your chances. The correct number was: {a}')
+        if get_to_next_move(b, a, number_of_set):
             break
         else:
-            print(f'Round number: {number_of_set}, you have {20 - number_of_set} rounds to play!')
-            # keep_on_playing = input('Do you want to keep on playing? Yes or No: ')
-            # if keep_on_playing == 'No':
-            # with open('saved_user_number', 'wb')as f:
-            # pickle.dump(b, f)
-            # print(f'user number saved: {b}')
-            # break
-            # else:
-            # continue
+            if a == b:
+                print(f'You had the correct number: {a}...You Won! Mazal Tov!')
+                break
+            elif a != b and number_of_set == 20:
+                print(f'sorry...end of the game, you missed all your chances. The correct number was: {a}')
+                break
+            else:
+                print(f'Round number: {number_of_set}, you have {20 - number_of_set} rounds to play!')
+
+
+def get_to_next_move(secret_number, player_data, game_set):
+    user_answer = input('If you want to save your game and come back later, type YES if not type NO: ')
+    while True:
+        if user_answer == 'YES':
+            with open('saved_info', 'wb')as f:
+                pickle.dump(secret_number, f)
+                pickle.dump(player_data, f)
+                pickle.dump(game_set, f)
+                print(f'Your game is saved, it was your round number:{game_set}')
+                return True
+        else:
+            return False
 
 
 play_the_game()
