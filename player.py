29ea486c04f8cd76b100ae80c088ab8eda2ec4ab
DvorahC <51906224+DvorahC@users.noreply.github.com>
2019-09-11 00:02:02 +0300

    Update player.py

--- player.py
+++ player.py
@@ -4,20 +4,23 @@ import re
 class Player:
 
     def __init__(self):
+        pass
 
-        def set_up_user_number(self):
-            while True:
-                user_number = input('enter a 4 digits number: ')
-                if verify_number_format(user_number):
-                    user_numbers_to_list = [int(digit) for digit in user_number]
-                    print(f'Your number: {user_numbers_to_list}')
-                    return user_numbers_to_list
-                else:
-                    continue
-
-        def verify_number_format(self):
-            if re.match(r'^\d{4}$', self) and not re.match(r'(\d).*\1', self):
-                return True
+    def set_up_user_number(self):
+        while True:
+            user_number = input('enter a 4 digits number: ')
+            if self.verify_number_format(user_number):
+                user_numbers_to_list = [int(digit) for digit in user_number]
+                print(f'Your number: {user_numbers_to_list}')
+                return user_numbers_to_list
             else:
-                print("This is not 4 digits number without repetition...Try again.")
-                return False
+                continue
+
+    def verify_number_format(self, n):
+        if re.match(r'^\d{4}$', n) and not re.match(r'(\d).*\1', n):
+            return True
+        else:
+            print("This is not 4 digits number without repetition...Try again.")
+            return False
+
+
